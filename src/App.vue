ca0d260b0cf48ea04f10186a68e094768d3cc1bd
Ilyukina <anyailyukina@yandex.ru>
2023-07-07 00:21:11 +0800

    init Reveal Elemets On Scroll

---
+++ src/App.vue
@@ -0,0 +1,143 @@
+<script setup>
+// import { scrollReveal } from 'vue-scroll-reveal';
+import {
+  faInstagram,
+  faFacebook,
+  faYoutube,
+  faTwitter,
+  faTelegram,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { createScrollRevealDirective } from "vue-scroll-reveal";
+const vScrollReveal = createScrollRevealDirective({
+  distance: "80px",
+  delay: 400,
+  duration: 2500,
+});
+
+//scrollReveal().reveal('media-icons i', { delay: 500, origin: 'bottom', interval: 200 })
+</script>
+
+<template>
+  <section class="sec-1" v-scroll-reveal="{ delay: 600, origin: 'bottom' }">
+    <div class="container">
+      <h2 class="main-title" v-scroll-reveal="{ delay: 500, origin: 'left' }">
+        Reveal Elemets On Scroll
+      </h2>
+      <div class="content">
+        <div class="image" v-scroll-reveal="{ delay: 600, origin: 'bottom' }">
+          <img src="./assets/image.png" alt="" />
+        </div>
+        <div class="text-box" v-scroll-reveal="{ delay: 700, origin: 'right' }">
+          <h3>Lorem ipsum</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+            consequuntur ea rerum qui dolorum laudantium officiis nam minus
+            quidem vel modi in accusamus commodi, iusto autem iste. Atque,
+            voluptatum inventore. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. At consequuntur ea rerum qui dolorum laudantium
+            officiis nam minus quidem vel modi in accusamus commodi, iusto autem
+            iste. Atque, voluptatum inventore.
+          </p>
+        </div>
+      </div>
+      <div class="media-icons">
+        <li v-scroll-reveal="{ delay: 500, origin: 'bottom' }">
+          <a href="#">
+            <svg-icon class="icon" :fa-icon="faTelegram" :size="30"></svg-icon>
+          </a>
+        </li>
+        <li  v-scroll-reveal="{ delay: 700, origin: 'bottom' }">
+          <a href="#">
+            <svg-icon class="icon" :fa-icon="faInstagram" :size="30"></svg-icon>
+          </a>
+        </li>
+        <li  v-scroll-reveal="{ delay: 1100, origin: 'bottom' }">
+          <a href="#">
+            <svg-icon class="icon" :fa-icon="faTwitter" :size="30"></svg-icon>
+          </a>
+        </li>
+        <li v-scroll-reveal="{ delay: 1300, origin: 'bottom' }">
+          <a href="#" >
+            <svg-icon class="icon" :fa-icon="faYoutube" :size="30"></svg-icon>
+          </a>
+        </li>
+      </div>
+    </div>
+  </section>
+
+  <section class="sec-2" v-scroll-reveal="{ delay: 500, origin: 'top' }">
+    <div class="container">
+      <h3
+        class="section-title"
+        v-scroll-reveal="{ delay: 500, origin: 'left' }"
+      >
+        Lorem Ipsum
+      </h3>
+      <div class="content">
+        <div class="image" v-scroll-reveal="{ delay: 500, origin: 'top' }">
+          <img src="./assets/image.png" alt="" />
+        </div>
+        <div class="info" v-scroll-reveal="{ delay: 500, origin: 'bottom' }">
+          <h4 class="info-title">Description</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+            consequuntur ea rerum qui dolorum laudantium officiis nam minus
+            quidem vel modi in accusamus commodi, iusto autem iste. Atque,
+            voluptatum inventore. Lorem ipsum dolor sit amet
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="sec-3" v-scroll-reveal="{ delay: 500, origin: 'top' }">
+    <div class="container">
+      <h3
+        class="section-title"
+        v-scroll-reveal="{ delay: 500, origin: 'left' }"
+      >
+        Lorem Ipsum
+      </h3>
+      <div class="content">
+        <div class="media-info">
+          <li v-scroll-reveal="{ delay: 500, origin: 'left' }">
+            <a href="#">
+              <svg-icon class="icon" :fa-icon="faTelegram" :size="30" />
+              faTelegram
+            </a>
+          </li>
+          <li v-scroll-reveal="{ delay: 700, origin: 'left' }">
+            <a href="#">
+              <svg-icon class="icon" :fa-icon="faInstagram" :size="30" />
+              faInstagram
+            </a>
+          </li>
+          <li v-scroll-reveal="{ delay: 900, origin: 'left' }">
+            <a href="#">
+              <svg-icon class="icon" :fa-icon="faFacebook" :size="30" />
+              faFacebook
+            </a>
+          </li>
+          <li v-scroll-reveal="{ delay: 1100, origin: 'left' }">
+            <a href="#">
+              <svg-icon class="icon" :fa-icon="faTwitter" :size="30" />
+              faTwitter
+            </a>
+          </li>
+          <li v-scroll-reveal="{ delay: 1300, origin: 'left' }">
+            <a href="#">
+              <svg-icon class="icon" :fa-icon="faYoutube" :size="30" />
+              faYoutube
+            </a>
+          </li>
+        </div>
+        <div class="image" v-scroll-reveal="{ delay: 500, origin: 'top' }">
+          <img src="./assets/image.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import SvgIcon from "vue3-icon";
+
+const app = createApp(App);
+app.component("svg-icon", SvgIcon);
+
+app.mount("#app");
